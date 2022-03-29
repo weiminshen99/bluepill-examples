3d6591e60f1ef077f033769bfdc36a5b97989967
Wingman <weiminshen99@gmail.com>
2022-03-29 16:42:56 -0700

    add serial-blink

---
+++ serial-blink/README.md
@@ -0,0 +1,32 @@
+# AARI Corporation, All Right Served
+# Author Wei-Min Shen, 3-10-2020
+
+===== HARDWARE =====
+
+for programming:
+MAC<-(usb)->[STLINK/V2]<-(swd)->[Bluepill/STM32F103C8]<-(pc13)->LED
+
+for communication:
+MAC<-(usb)->[USB-Serial-Cable]<-(uart1)->[Bluepill/STM32F103C8]
+
+===== SOFTWARE (see Makefile) =====
+
+arm-none-eabi-{gcc,ldf,as}
+ST-Flash utility (https://github.com/texane/stlink)
+
+===== Operation Procedure =====
+
+0. Type "make" to make $(TARGET).{elf,bin}
+1. Type "make flash" to load into Bluepill via STLINK V2 dongle
+2. Now, you shall see the LED blinking on Bluepill
+3. Replace STLINK by a USB-Serial cable to USART1 on Bluepill
+   MAC <-> [USB-Serial-Cable] <-(USART1)-> [PA9,PA10,G,5V] Bluepill
+5. On MAC, run "ls /dev/cu.usbserial-*" to identify the serial port
+6. On MAC, run "screen /dev/cu.usbserial-1410 115200"
+   or  "minicom -b 115200 -o -D /dev/cu.usbserial-1410"
+7. You should see the printings on the terminal
+8. You can type in a number [0..9] to change the blink period
+9. Press the reset button on Bluepill to continue (to be improved)
+
+===== THE END =====
+
---
+++ serial-blink/main.c
@@ -0,0 +1,47 @@
+#include "stm32f1.h"
+
+static int blink_period = 45000;
+
+static void setup(void) {
+
+  RCC->APB2ENR |= BIT(4);                // GPIOC, for LED on PC13
+  SET_PIN_MODE(GPIOC, LED_PIN, 0b0110);  // open drain, output 2
+
+  RCC->APB2ENR |= BIT(2);   // GPIOA, for USART1 TX/RX on PA9 and PA10
+  RCC->APB2ENR |= BIT(14);  // enable clock on USART1
+  SET_PIN_MODE(GPIOA, TX_PIN, 0b1010);     // TX pin mode = AF, push/pull
+  SET_PIN_MODE(GPIOA, RX_PIN, 0b0100);     // RX pin mode = input, floating
+  UART1->BRR = 0x45;                       // Set baud rate, TRM 27.3.4
+  UART1->CR1 = BIT(13) | BIT(2) | BIT(3);  // Enable USART1
+}
+
+static void uart_putc(struct uart *uart, uint8_t byte) {
+  uart->DR = byte;                  // Write next byte
+  do {
+  } while ((uart->SR & BIT(7)) == 0);  // Wait until byte is scheduled
+}
+
+int main(void) {
+  setup();
+  volatile int count = 0, led_on = 0;
+
+  for (;;) {
+    if (UART_HAS_DATA(UART1)) {
+      int ch = UART_READ(UART1);
+      uart_putc(UART1, ch);
+      if (ch >= '0' && ch <= '9') blink_period = (ch - '0' + 1) * 150000;
+    }
+    if (++count > blink_period) {
+      count = 0;
+      led_on = !led_on;
+      GPIOC->BSRR |= BIT(LED_PIN + (led_on ? 0 : 16));
+      uart_putc(UART1, led_on ? '1' : '0');
+    }
+  }
+
+  return 0;
+}
+
+void *_sbrk() {
+  return 0;
+}
